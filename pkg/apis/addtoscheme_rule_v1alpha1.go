48e9e540c752cd9f52b2a33f5d808f5cee83a66a
tchughesiv <tchughesiv@gmail.com>
2019-11-25 11:21:43 -0600

    add other schemas of interest
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- pkg/apis/addtoscheme_rule_v1alpha1.go
+++ pkg/apis/addtoscheme_rule_v1alpha1.go
@@ -3,8 +3,13 @@ package apis
 import (
 	"os"
 
+	oappsv1 "github.com/openshift/api/apps/v1"
+	buildv1 "github.com/openshift/api/build/v1"
+	oimagev1 "github.com/openshift/api/image/v1"
+	routev1 "github.com/openshift/api/route/v1"
 	"github.com/tchughesiv/inferator/pkg/apis/rule/v1alpha1"
 	"github.com/tchughesiv/inferator/pkg/controller/operationrule/constants"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -16,7 +21,12 @@ func init() {
 	AddToSchemes = append(AddToSchemes,
 		v1alpha1.SchemeBuilder.AddToScheme,
 		corev1.SchemeBuilder.AddToScheme,
+		appsv1.SchemeBuilder.AddToScheme,
 		rbacv1.SchemeBuilder.AddToScheme,
+		oappsv1.AddToScheme,
+		routev1.AddToScheme,
+		oimagev1.AddToScheme,
+		buildv1.AddToScheme,
 	)
 	if os.Getenv(constants.RuntimeEnv) == "true" {
 		schemeGroupVersion := schema.GroupVersion{Group: os.Getenv("OPRULE_OBJECT_GROUP"), Version: os.Getenv("OPRULE_OBJECT_VERSION")}
